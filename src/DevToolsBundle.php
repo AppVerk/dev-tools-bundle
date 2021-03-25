dd0d5a072e723fb3458cf4211962f5d1a11b95ac
Shostak <sergii.shostak@appverk.com>
2021-03-25 13:55:46 +0100

    Add doctrine dependency

--- src/DevToolsBundle.php
+++ src/DevToolsBundle.php
@@ -5,8 +5,6 @@ declare(strict_types = 1);
 namespace DevTools;
 
 use DevTools\DependencyInjection\Compiler\AccessControlHandlerPass;
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
-use ReflectionClass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -17,35 +15,5 @@ class DevToolsBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new AccessControlHandlerPass());
-
-        $this->addRegisterMappingsPass($container);
-        $this->registerJWTMappingsPass($container);
-    }
-
-    private function addRegisterMappingsPass(ContainerBuilder $container): void
-    {
-        $currentDirectory = realpath(__DIR__);
-        $directories = [];
-        $aliasMap = [];
-    }
-
-    private function registerJWTMappingsPass(ContainerBuilder $container): void
-    {
-        $bundles = $container->getParameter('kernel.bundles');
-        $class = new ReflectionClass(GesdinetJWTRefreshTokenBundle::class);
-
-        if (isset($bundles[$class->getShortName()])) {
-            return;
-        }
-
-        $namespace = $class->getNamespaceName() . '\Entity';
-        $path = dirname($class->getFileName());
-
-        $mappings = [
-            $path . '/Resources/config/orm/doctrine-orm' => $namespace,
-            $path . '/Resources/config/orm/doctrine-superclass' => $namespace,
-        ];
-
-        $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
     }
 }
