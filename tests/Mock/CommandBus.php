8ad1fe0074f9a5948dfc0b5db71f2507d8036066
Shostak <sergii.shostak@appverk.com>
2021-04-23 17:28:15 +0200

    NSZ-39 - Update tests documentation

---
+++ tests/Mock/CommandBus.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DevTools\Tests\Mock;
+
+use DevTools\Messenger\CommandBus as BaseCommandBus;
+
+class CommandBus extends BaseCommandBus
+{
+    private bool $enabled = true;
+
+    private array $queue = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function dispatch(object $command)
+    {
+        if ($this->enabled) {
+            return parent::dispatch($command);
+        }
+
+        $this->queue[] = $command;
+    }
+
+    public function enable(): void
+    {
+        $this->enabled = true;
+
+        foreach ($this->queue as $command) {
+            parent::dispatch($command);
+        }
+    }
+
+    public function disable(): void
+    {
+        $this->enabled = false;
+    }
+}
