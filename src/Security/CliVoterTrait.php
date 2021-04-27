bf8ebceab9a2986c986c42c38dfc0212b955d31f
Shostak <sergii.shostak@appverk.com>
2021-04-27 16:48:29 +0200

    Fix fos rest docs

--- src/Security/CliVoterTrait.php
+++ src/Security/CliVoterTrait.php
@@ -4,10 +4,13 @@ declare(strict_types = 1);
 
 namespace DevTools\Security;
 
+use Symfony\Component\Security\Core\Authentication\Token\NullToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
 trait CliVoterTrait
 {
-    public function isRunningInCli(): bool
+    public function isRunningInCli(TokenInterface $token): bool
     {
-        return \PHP_SAPI === 'cli';
+        return \PHP_SAPI === 'cli' && $token instanceof NullToken;
     }
 }
