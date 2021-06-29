bb740e6e48a692c5e18d8dae4261ce735cc21a71
Shostak <sergii.shostak@appverk.com>
2021-06-29 14:10:01 +0200

    Add possibility to mock dates in DateTimeProvider

--- src/Domain/DateTimeProvider.php
+++ src/Domain/DateTimeProvider.php
@@ -6,8 +6,20 @@ namespace DevTools\Domain;
 
 class DateTimeProvider
 {
+    private static ?\DateTimeImmutable $currentDate = null;
+
     public static function current(): \DateTimeImmutable
     {
-        return new \DateTimeImmutable();
+        return self::$currentDate ?? new \DateTimeImmutable();
+    }
+
+    public static function setCurrentDate(\DateTimeImmutable $date): void
+    {
+        self::$currentDate = $date;
+    }
+
+    public static function reset(): void
+    {
+        self::$currentDate = null;
     }
 }
