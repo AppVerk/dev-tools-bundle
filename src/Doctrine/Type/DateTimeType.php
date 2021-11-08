1b52d8bed27323baa5a01677f3012bd7123279cc
Sergii Shostak <shostak.sergii@gmail.com>
2021-11-08 12:48:27 +0100

    Drop timostamm/doctrine-fixed-timezone dependency to unblock doctrine updates, add replacement for timostamm functionalities

---
+++ src/Doctrine/Type/DateTimeType.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DevTools\Doctrine\Type;
+
+use DateTime;
+use DateTimeInterface;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType as Base;
+
+class DateTimeType extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return $value;
+        }
+
+        if ($value instanceof DateTimeInterface) {
+            $value = TimeZoneConverter::convertToDb($value);
+
+            return $value->format($platform->getDateTimeFormatString());
+        }
+
+        throw ConversionException::conversionFailedInvalidType(
+            $value,
+            $this->getName(),
+            ['null', DateTime::class]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof DateTimeInterface) {
+            return $value;
+        }
+
+        $dateTime = DateTime::createFromFormat($platform->getDateTimeFormatString(), $value, TimeZoneConverter::dbTimeZone());
+
+        if (!$dateTime) {
+            throw ConversionException::conversionFailedFormat($value, $this->getName(), $platform->getDateTimeFormatString());
+        }
+
+        return $dateTime;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+}
