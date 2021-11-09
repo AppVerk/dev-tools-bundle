9c90c94fceaef7fb236dd0d9c3353f11fd60bc25
Sergii Shostak <shostak.sergii@gmail.com>
2021-11-09 15:16:10 +0100

    Fix typo in PaginatedResult, replace deprecated methods in AbstractRepository

--- src/Repository/PaginatedResult.php
+++ src/Repository/PaginatedResult.php
@@ -6,24 +6,24 @@ namespace DevTools\Repository;
 
 class PaginatedResult extends AbstractPageResult
 {
-    private int $totalItemsCount;
+    private int $itemsTotalCount;
 
     private int $currentPage;
 
     private int $pageCount;
 
-    public function __construct(array $items, int $totalItemsCount, int $currentPage, int $pageCount, int $itemsPerPage)
+    public function __construct(array $items, int $itemsTotalCount, int $currentPage, int $pageCount, int $itemsPerPage)
     {
         parent::__construct($items, $itemsPerPage);
 
-        $this->totalItemsCount = $totalItemsCount;
+        $this->itemsTotalCount = $itemsTotalCount;
         $this->currentPage = $currentPage;
         $this->pageCount = $pageCount;
     }
 
-    public function getTotalItemsCount(): int
+    public function getItemsTotalCount(): int
     {
-        return $this->totalItemsCount;
+        return $this->itemsTotalCount;
     }
 
     public function getCurrentPage(): int
